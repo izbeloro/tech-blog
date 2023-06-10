1756099af349080ebc7cd30cdc52e95cf8858724
Samantha Orozco <orozcoxs@gmail.com>
2023-06-10 11:47:26 -0700

    port changed again

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers })
 
 const sess = {
-    secret: 'Super secret',
+    secret: 'Super secret secret',
     cookie: {},
     resave: false,
     saveUninitialized: true,
