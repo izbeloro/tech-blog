7e8ef3eb5b2283961fa8edeaa8e0f9f835dba081
Samantha Orozco <orozcoxs@gmail.com>
2023-06-10 13:25:46 -0700

    reverted to original code

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_MARIA_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_MARIA_URL);
 } else {
     sequelize = new Sequelize(
         process.env.DB_NAME,
@@ -13,7 +13,9 @@ if (process.env.JAWSDB_URL) {
         {
             host: 'localhost',
             dialect: 'mysql',
-            port: 3306
+            port: process.env.PORT
         }
     );
 }
+
+module.exports = sequelize;
